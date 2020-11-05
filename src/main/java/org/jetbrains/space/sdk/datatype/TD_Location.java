715e286c83c37298235862b745dbd8d27ec71a8b
Peter Gromov <peter@jetbrains.com>
2020-11-05 13:29:16 +0100

    add TD_Location.belongsToLocation

--- src/main/java/org/jetbrains/space/sdk/datatype/TD_Location.java
+++ src/main/java/org/jetbrains/space/sdk/datatype/TD_Location.java
@@ -1,15 +1,20 @@
 package org.jetbrains.space.sdk.datatype;
 
+import org.jetbrains.annotations.NotNull;
+
 public class TD_Location implements SpaceObject {
 
-    public final String id;
-    public final String name;
-    public final TD_Location parent;
+  public final String id;
+  public final String name;
+  public final TD_Location parent;
 
-    private TD_Location(String id, String name, TD_Location parent) {
-        this.id = id;
-        this.name = name;
-        this.parent = parent;
-    }
+  private TD_Location(String id, String name, TD_Location parent) {
+    this.id = id;
+    this.name = name;
+    this.parent = parent;
+  }
 
+  public boolean belongsToLocation(@NotNull String locationId) {
+    return locationId.equals(id) || parent != null && parent.belongsToLocation(locationId);
+  }
 }
