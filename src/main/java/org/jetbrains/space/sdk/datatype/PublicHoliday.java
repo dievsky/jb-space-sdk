aa5ad13b870051d43463644223789e382541b155
Peter Gromov <peter@jetbrains.com>
2022-01-04 10:08:26 +0300

    timesheets: honor half-day holidays

--- src/main/java/org/jetbrains/space/sdk/datatype/PublicHoliday.java
+++ src/main/java/org/jetbrains/space/sdk/datatype/PublicHoliday.java
@@ -8,12 +8,13 @@ public class PublicHoliday implements SpaceObject {
     public final String name;
     public final LocalDate date;
     public final boolean workingDay;
+    public final boolean halfDay;
 
-    public PublicHoliday(String id, String name, LocalDate date, boolean workingDay) {
+    public PublicHoliday(String id, String name, LocalDate date, boolean workingDay, boolean halfDay) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.workingDay = workingDay;
+        this.halfDay = halfDay;
     }
-
 }
