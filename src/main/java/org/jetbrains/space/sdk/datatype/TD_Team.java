eedbb74681e419a47f48ca89f871841ed67e6899
dievsky <dievsky@gmail.com>
2022-01-13 13:55:58 +0100

    Added hierarchy method for TD_Team

--- src/main/java/org/jetbrains/space/sdk/datatype/TD_Team.java
+++ src/main/java/org/jetbrains/space/sdk/datatype/TD_Team.java
@@ -1,16 +1,25 @@
 package org.jetbrains.space.sdk.datatype;
 
+import java.util.stream.Stream;
+
 public class TD_Team implements SpaceObject {
   public final String id;
   public final String name;
+  public final TD_Team parent;
 
-  public TD_Team(String id, String name) {
+  public TD_Team(String id, String name, TD_Team parent) {
     this.id = id;
     this.name = name;
+    this.parent = parent;
   }
 
   @Override
   public String toString() {
     return name;
+  }
+
+  public Stream<TD_Team> hierarchy() {
+    Stream<TD_Team> me = Stream.of(this);
+    return parent == null ? me : Stream.concat(me, parent.hierarchy());
   }
 }
